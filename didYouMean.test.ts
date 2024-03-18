a91e92b64e0d6355487047592ddd9d828b21465a
Simon Lagos <simon.lagos.sallhed@gmail.com>
2024-03-18 23:07:36 +0100

    Add test for didYouMean

---
+++ didYouMean.test.ts
@@ -0,0 +1,15 @@
+import { expect, test, vi } from 'vitest'
+import didYouMean from './src/didYouMean.js'
+import chalk from 'chalk'
+
+test('did-you-mean', () => {
+  const consoleMock = vi
+    .spyOn(console, 'log')
+    .mockImplementation(() => undefined)
+  const possibleValues = ['yellow', 'blue', 'green']
+  const value = 'red'
+  didYouMean(value, possibleValues)
+  expect(consoleMock).toHaveBeenLastCalledWith(
+    `\nDid you mean?\n${chalk.bold('green')}\n`,
+  )
+})
